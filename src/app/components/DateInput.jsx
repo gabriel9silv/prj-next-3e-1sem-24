bf042a11bf82ead932c0b500b7eeb7ff40a24742
gabriel9silv <134931559+gabriel9silv@users.noreply.github.com>
2024-08-12 11:26:29 -0300

    erro

---
+++ src/app/components/DateInput.jsx
@@ -0,0 +1,10 @@
+// components/DateInput.jsx
+import React from 'react';
+
+const DateInput = () => {
+  return (
+    <input type="date" className="input" />
+  );
+};
+
+export default DateInput;
---
+++ src/app/components/Horainicio.jsx
@@ -0,0 +1,10 @@
+// components/Horainicio.jsx
+import React from 'react';
+
+const Horainicio = () => {
+  return (
+    <input type="time" className="input" />
+  );
+};
+
+export default Horainicio;
